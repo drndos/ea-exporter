005de4d40ef56a3084d3cdb0af3920f4e7f0d8f4
Filip Bednárik <bednarik@essential-data.sk>
2017-05-05 14:59:56 +0200

    Add SVN support

--- src/main/java/sk/essentialdata/EaDumpService.java
+++ src/main/java/sk/essentialdata/EaDumpService.java
@@ -15,9 +15,16 @@ public class EaDumpService {
     @Value("${app.connection-string}")
     private String connectionString;
 
+    @Value("${app.isSVN}")
+    private boolean isSVN;
+
     public void dump(String guid){
         org.sparx.Repository r = new org.sparx.Repository();
         r.OpenFile(connectionString);
+        if(isSVN)
+        {
+            r.GetPackageByGuid(guid).VersionControlGetLatest(false);
+        }
         r.GetProjectInterface().RunHTMLReport(guid, pathToWebServer, "PNG", "<default>", ".html");
         r.CloseFile();
     }
